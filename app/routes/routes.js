f32d71b09ad14c616a129f7e9b9834082117f8d7
Manuel Abrante <mabrante@socialh4ck.com>
2016-05-15 14:14:38 -0400

    call users on the router

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,11 @@
 var express = require('express'),
   router = express.Router();
 
-function Router(app) {
+var Users = require('../controllers/users');
 
+function Router(app, passport) {
+
+  app.use('/users', Users(router));
   //The 404 Route (ALWAYS Keep this as the last route)
   app.use(function(req, res) {
     res.status(404).send({ code: 404, success: false, key: true, message: req.trans('not_found') });
