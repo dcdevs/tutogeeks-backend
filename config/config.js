c71d13728b5e2af864858212c50b2f15838f05be
Manuel Abrante <mabrante@socialh4ck.com>
2016-05-14 20:42:05 -0400

    added config file

---
+++ config/config.js
@@ -0,0 +1,19 @@
+var env = process.env.NODE_ENV || 'local';
+
+
+var config = {
+  // local config
+  local: {
+    tokenSecret: 'pdysgd62cl3car1kyb9o6qhdactmd9zp3dnp14i',
+    db: {
+      password: '',
+      username: '',
+      host: 'localhost',
+      database: 'tutogeeks',
+      port: 27017
+    }
+  }
+};
+
+
+module.exports = config[env];
