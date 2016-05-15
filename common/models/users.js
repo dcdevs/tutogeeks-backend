ba015cb075bc68c18be2dfd6a013d8b922a2c03d
Manuel Abrante <mabrante@socialh4ck.com>
2016-05-15 14:13:44 -0400

    set User Methods for password hash and validate

--- common/models/users.js
+++ common/models/users.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+var bcrypt = require('bcrypt-nodejs');
+
 var UsersSchema = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, require: true },
@@ -10,7 +12,7 @@ var UsersSchema = new Schema({
   activated: { type: Boolean, default: false },
   type: { type: String, enum: ['owner', 'guest', 'partner'] },
   banned: { type: Boolean, default: false },
-  services: {} // TODO , set by ID or set object with all the properties
+  services: {}, // TODO , set by ID or set object with all the properties
   profile: {
     firstName: { type: String, required: true },
     lastName: String,
@@ -32,5 +34,17 @@ var UsersSchema = new Schema({
     }
   }
 });
+
+
+// generating a hash
+UsersSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+UsersSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 
 module.exports = mongoose.model('User', UsersSchema);
