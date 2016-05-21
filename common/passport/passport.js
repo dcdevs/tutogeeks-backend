2ed053423bad9c20f3b461a47b50da1d7dc0118b
Manuel Abrante <mabrante@socialh4ck.com>
2016-05-21 18:30:43 -0400

    create passport session

--- common/passport/passport.js
+++ common/passport/passport.js
@@ -1,13 +1,13 @@
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/users');
-
+var _ = require('lodash');
 
 
 module.exports = function(passport) {
 
   // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
-    done(null, user.id);
+    done(null, user._id);
   });
 
   // used to deserialize the user
@@ -17,12 +17,29 @@ module.exports = function(passport) {
     });
   });
 
-  // create session
-  passport.use('local-signup', new LocalStrategy(function(username, password, done) {
+  passport.use('local', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true // allows us to pass back the entire request to the callback
+  }, function(req, username, password, done) {
+
     process.nextTick(function() {
 
+      var user = _.clone(req.tryTologin);
+
+      if (!user.validPassword(password))
+        return done(null, false, 'invalid password');
+
+
+      user = user.toObject();
+      delete user.password;
+
+      req.tryTologin = undefined;
+
+      return done(null, user);
 
     });
-  }));
+
+  }))
 
 };
